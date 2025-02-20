86e1ed301506981ea2817069e8da4f4f25f8a2c8
Chip Wiley <chipwiley@outlook.com>
2025-02-20 10:05:15 -0800

    Add file chooser

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,19 +1,57 @@
 import Versions from './components/Versions'
 import ImageEditor from './components/ImageEditor'
+import { useState } from 'react'
 
 function App(): JSX.Element {
-  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const [selectedImage, setSelectedImage] = useState<string>('/media/DSC_0374.JPG')
+
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = event.target.files?.[0]
+    if (file) {
+      // const imageUrl = URL.createObjectURL(file)
+      setSelectedImage(`/media/${file.name}`)
+    }
+  }
 
   return (
     <>
       <div className="text">
-        <span className="ts">WebGPU</span>
+        <span className="ts">WebGPU Saturation Adjustment</span>
       </div>
-      <div className="title">Saturation Adjustment</div>
+      <div className="title">{selectedImage}</div>
+
       <div className="image-container">
-        <img src="/DSC_0374.JPG" style={{ borderRadius: '5px', width: '400px', height: '400px' }} />
-        <ImageEditor grayscaleFactor={0.0} src="/DSC_0374.JPG" width={400} height={400} />
+        <img src={selectedImage} style={{ borderRadius: '5px', width: '400px', height: '400px' }} />
+        <ImageEditor grayscaleFactor={0.0} src={selectedImage} width={400} height={400} />
       </div>
+
+      <div style={{ textAlign: 'center' }}>
+        <label
+          htmlFor="file-upload"
+          style={{
+            display: 'inline-block',
+            marginBottom: '1rem',
+            padding: '8px 16px',
+            backgroundColor: '#2c2c2c',
+            border: 'none',
+            borderRadius: '20px',
+            color: '#888',
+            cursor: 'pointer'
+          }}
+        >
+          Select Image
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleFileSelect}
+          style={{
+            display: 'none'
+          }}
+        />
+      </div>
+
       <Versions></Versions>
     </>
   )
